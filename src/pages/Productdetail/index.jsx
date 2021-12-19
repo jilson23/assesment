0521e67a904245a56e06f1b761ce8aa181288338
jilson jimenez <jilsonjimenez@gmail.com>
2021-12-19 17:05:45 -0500

    styling productDetail

--- src/pages/Productdetail/index.jsx
+++ src/pages/Productdetail/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductsId } from '../../services/products';
+import './style.css';
 
 const Productdetail = () => {
   const [products, setProducts] = useState([]);
@@ -15,14 +16,34 @@ const Productdetail = () => {
   }, []);
   return (
     <div className="productDetail">
-      <h3>hola{products.title}</h3>
-      {products.id}
-      {products.price}
-      {products.description}
-      {products.category}
-      {products.image}
-      {products.rating?.rate}
-      {products.rating?.count}
+      <div className="productDetail__containerImage">
+        <img className="productDetail__image" src={products.image} alt="" />
+      </div>
+      <div className="productDetail__info">
+        <h2 className="productDetail__title">
+          {products.title}, <small>Id: {products.id}</small>
+        </h2>
+        <p>
+          <b>Description: </b>
+          {products.description}
+        </p>
+        <p>
+          <b>Price: </b>
+          {products.price}
+        </p>
+        <p>
+          <b>Category: </b>
+          {products.category}
+        </p>
+        <p>
+          <b>Rate: </b>
+          {products.rating?.rate}
+        </p>
+        <p>
+          <b>Count: </b>
+          {products.rating?.count}
+        </p>
+      </div>
     </div>
   );
 };
