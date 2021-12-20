e5f1a2ab928eeaf1ca667d8d83124b0604e14f16
jilson jimenez <jilsonjimenez@gmail.com>
2021-12-20 10:18:11 -0500

    Add Loading in product detail

--- src/pages/Productdetail/index.jsx
+++ src/pages/Productdetail/index.jsx
@@ -16,34 +16,40 @@ const Productdetail = () => {
   }, []);
   return (
     <div className="productDetail">
-      <div className="productDetail__containerImage">
-        <img className="productDetail__image" src={products.image} alt="" />
-      </div>
-      <div className="productDetail__info">
-        <h2 className="productDetail__title">
-          {products.title}, <small>Id: {products.id}</small>
-        </h2>
-        <p>
-          <b>Description: </b>
-          {products.description}
-        </p>
-        <p>
-          <b>Price: </b>
-          {products.price}
-        </p>
-        <p>
-          <b>Category: </b>
-          {products.category}
-        </p>
-        <p>
-          <b>Rate: </b>
-          {products.rating?.rate}
-        </p>
-        <p>
-          <b>Count: </b>
-          {products.rating?.count}
-        </p>
-      </div>
+      {products.length < 1 ? (
+        <h1 className="productDetail__loading">Loading...</h1>
+      ) : (
+        <>
+          <div className="productDetail__containerImage">
+            <img className="productDetail__image" src={products.image} alt="" />
+          </div>
+          <div className="productDetail__info">
+            <h2 className="productDetail__title">
+              {products.title}, <small>Id: {products.id}</small>
+            </h2>
+            <p>
+              <b>Description: </b>
+              {products.description}
+            </p>
+            <p>
+              <b>Price: </b>
+              {products.price}
+            </p>
+            <p>
+              <b>Category: </b>
+              {products.category}
+            </p>
+            <p>
+              <b>Rate: </b>
+              {products.rating?.rate}
+            </p>
+            <p>
+              <b>Count: </b>
+              {products.rating?.count}
+            </p>
+          </div>
+        </>
+      )}
     </div>
   );
 };
