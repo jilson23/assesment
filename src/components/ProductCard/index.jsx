03470689f5b67548ab8d8cb171626fef80eee0df
jilson jimenez <jilsonjimenez@gmail.com>
2021-12-20 15:21:40 -0500

    secont to hour, min, and secont, and random number

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,13 +1,29 @@
-/* eslint-disable prefer-const */
+/* eslint-disable */
 import './style.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { useRef, useEffect, useState } from 'react';
 
 const ProductCard = ({ title, image, id }) => {
-  const [time, setTime] = useState(10);
+  const random = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  const [time, setTime] = useState(random(5, 80));
+
   let intervalRef = useRef();
   const decreaseNum = () => setTime((prev) => prev - 1);
+
+  const secondsToString = (seconds) => {
+    let hour = Math.floor(seconds / 3600);
+    hour = hour < 10 ? '0' + hour : hour;
+    let minute = Math.floor((seconds / 60) % 60);
+    minute = minute < 10 ? '0' + minute : minute;
+    let second = seconds % 60;
+    second = second < 10 ? '0' + second : second;
+    return hour + ':' + minute + ':' + second;
+  };
+
   useEffect(() => {
     if (time > 0) {
       intervalRef.current = setInterval(decreaseNum, 1000);
@@ -24,7 +40,7 @@ const ProductCard = ({ title, image, id }) => {
         </div>
         <div className="productCard__footer">
           {/* <span className="productCard__counter">00:00:00</span> */}
-          <span className="productCard__counter">{time}</span>
+          <span className="productCard__counter">{secondsToString(time)}</span>
           {time > 0 ? (
             <Link className="productCard__link" to={`/productdetail/${id}`}>
               Go to Details
